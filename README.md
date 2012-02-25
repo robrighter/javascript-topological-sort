7942608ed14a2c548a14ff43ee24330826304f1d
Rob Righter <robrighter@gmail.com>
2012-02-25 18:54:26 -0500

    performance improvement by not rechecking already processed nodes

--- README.md
+++ README.md
@@ -4,5 +4,3 @@ Javascript Topological Sort
 Implementation of a topological sort in javascript. Topological sorts are useful for determining proper order precedence. To learn more about topological sorting watch this video: 
 
 http://video.google.com/videoplay?docid=2755333184412284530
-
-<embed id=VideoPlayback src=http://video.google.com/googleplayer.swf?docid=2755333184412284530&hl=en&fs=true style=width:400px;height:326px allowFullScreen=true allowScriptAccess=always type=application/x-shockwave-flash> </embed>
--- topological-sort.js
+++ topological-sort.js
@@ -1,20 +1,29 @@
 function topologicalSort(graph){
+	var numberOfNodes = graph.length;
 	var processed = [];
+	var unprocessed = [];
 	var queue = [];
-	populateIndegrees();
+	populateIndegreesAndUnprocessed();
 	processList();
 	return processed;
 	
 	function processList(){
-		for(var i=0; i<graph.length; i++){
-			if(graph[i].indegrees === 0){
-				queue.push(i);
-				graph[i].indegrees = -1; //dont look at this one anymore
+		var toBeRemoved = [];
+		for(var i=0; i<unprocessed.length; i++){
+			var nodeid = unprocessed[i];
+			if(graph[nodeid].indegrees === 0){
+				queue.push(nodeid);
+				console.log('About to remove edge ' + i + ' from the graph.');
+				unprocessed.splice(i, 1); //Remove this node, its all done.
+				i--;//decrement i since we just removed that index from the iterated list;
 			}
 		}
 		
+		console.log(unprocessed);
+		console.log('\n\n');
+		
 		processStartingPoint(queue.shift());
-		if(processed.length<graph.length){
+		if(processed.length<numberOfNodes){
 			processList();
 		}
 	}
@@ -24,28 +33,33 @@ function topologicalSort(graph){
 		if(i == undefined){
 			throw "You have a cycle!!";
 		}
-		graph[i].edges.forEach(function(e){
+		for( var t=0; t<graph[i].edges.length; t++){
+			var e = graph[i].edges[t];
 			graph[e].indegrees--;
-		});
+		};
 		processed.push(i);
 	}
 
 
-	function populateIndegrees(){
+	function populateIndegreesAndUnprocessed(){
 		for(var i=0; i<graph.length; i++){
+			unprocessed.push(i);
 			if(!graph[i].hasOwnProperty('indegrees')){
 				graph[i].indegrees = 0
 			}
-			graph[i].edges.forEach(function(e){
+			for( var t=0; t<graph[i].edges.length; t++){
+				var e = graph[i].edges[t];
 				if(!graph[e].hasOwnProperty('indegrees')){
 					graph[e].indegrees = 1
 				}
 				else{
 					graph[e].indegrees = graph[e].indegrees + 1;
-				} 
-			});
+				}
+			}
 		}
 	}
+	
+	
 }
 
 
